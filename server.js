28ff0d553f7b96d9b058bd0bd278b7ea073d6de2
Misha <73886737+misha244@users.noreply.github.com>
2021-05-23 18:47:59 +0100

    add cors

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const fs = require("fs");
+const cors = require("cors");
 
 const { htmlRouter, apiRouter } = require("./routes/indexRoutes");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
 app.use(express.static("public"));
+
 app.use("/api", apiRouter);
 app.use("/", htmlRouter);
 
