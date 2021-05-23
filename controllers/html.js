d0c9c33a0df6df84a6e2b3a207470f023851eebf
Misha <73886737+misha244@users.noreply.github.com>
2021-05-23 19:30:12 +0100

    swap fn, trying to fix savenote

--- controllers/html.js
+++ controllers/html.js
@@ -1,18 +1,15 @@
 const getStaticFilePath = require("../util/getStaticFile");
 
-const renderIndexPage = (req, res) => {
-  const filePath = getStaticFilePath("index");
-
-  res.sendFiles(filePath);
-};
-
 const renderNotesPage = (req, res) => {
   const filePath = getStaticFilePath("notes");
 
   res.sendFiles(filePath);
 };
 
-module.exports = {
-  renderIndexPage,
-  renderNotesPage,
+const renderIndexPage = (req, res) => {
+  const filePath = getStaticFilePath("index");
+
+  res.sendFiles(filePath);
 };
+
+module.exports = { renderNotesPage, renderIndexPage };
